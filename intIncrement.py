487efeee93aa835c37beeb7e1a720ebe7e9781bc
Damian Myerscough <Damian.Myerscough@gmail.com>
2017-04-13 22:22:15 -0700

    Fix flake

--- intIncrement.py
+++ intIncrement.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 '''
-Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
-You may assume the integer do not contain any leading zero, except the number 0 itself.
-The digits are stored such that the most significant digit is at the head of the list.
+Given a non-negative integer represented as a non-empty array of digits,
+plus one to the integer. You may assume the integer do not contain any
+leading zero, except the number 0 itself. The digits are stored such that
+the most significant digit is at the head of the list.
 '''
+
 
 def increment(arr, pos=-1):
     '''
@@ -28,6 +30,7 @@ def increment(arr, pos=-1):
         arr[pos] += 1
 
     return arr
+
 
 if __name__ == '__main__':
     import doctest
