e421063df09765a666fd57b8e0e3e21cae7c8875
Damian Myerscough <Damian.Myerscough@gmail.com>
2019-04-09 21:58:12 -0700

    Check to see if you can spell your name using the periodic table

---
+++ periodicName.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+elements = [
+    'h', 'he', 'li', 'be', 'b', 'c', 'n', 'o', 'f', 'ne', 'na', 'mg', 'al', 'si', 'p', 's', 'cl', 'ar',
+    'k', 'ca', 'sc', 'ti', 'v', 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga', 'ge', 'as', 'se', 'br',
+    'kr', 'rb', 'sr', 'y', 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn', 'sb', 'te',
+    'i', 'xe', 'cs', 'ba', 'la', 'ce', 'pr', 'nd', 'pm', 'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm',
+    'yb', 'lu', 'hf', 'ta', 'w', 're', 'os', 'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn',
+    'fr', 'ra', 'ac', 'th', 'pa', 'u', 'np', 'pu', 'am', 'cm', 'bk', 'cf', 'es', 'fm', 'md', 'no', 'lr',
+    'rf', 'db', 'sg', 'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og', 'e', 'g',
+    'l', 'm','a', 'z', 'j', 't', 'q', 'd', 'x', 'r'
+]
+
+
+def elementSpell(word, spelling=None):
+    if spelling is None:
+        spelling = []
+
+    for element in elements:
+        if element == word:
+            spelling.append(element)
+        elif element == word[:len(element)]:
+            spelling.append(element)
+            return elementSpell(word[len(element):], spelling)
+
+    return spelling
+
+
+print(elementSpell('damian'))
+print(elementSpell('jason'))
+
